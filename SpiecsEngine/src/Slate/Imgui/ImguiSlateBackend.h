a217c72c72a3ffe3f5bfc98e3e1b75902bc8d98d
Spiecs <spiecs@outlook.com>
2024-04-23 17:59:10 +0800

    Update ImguiSlateBackend.h

--- SpiecsEngine/src/Slate/Imgui/ImguiSlateBackend.h
+++ SpiecsEngine/src/Slate/Imgui/ImguiSlateBackend.h
@@ -2,6 +2,12 @@
 #include "Core/Core.h"
 #include "Render/FrameInfo.h"
 
+#include "Slate/Imgui/ImguiConsole.h"
+#include "Slate/Imgui/ImguiContentBrowser.h"
+#include "Slate/Imgui/ImguiProperty.h"
+#include "Slate/Imgui/ImguiStage.h"
+#include "Slate/Imgui/ImguiViewport.h"
+
 namespace Spiecs {
 
 	class ImguiSlateBackend
@@ -19,5 +25,11 @@ namespace Spiecs {
 		void DrawTest(FrameInfo& frameInfo);
 
 	private:
+		std::unique_ptr<ImguiViewport> m_ViewPort;
+		std::unique_ptr<ImguiStage> m_Stage;
+		std::unique_ptr<ImguiProperty> m_Property;
+		std::unique_ptr<ImguiContentBrowser> m_ContentBrowser;
+		std::unique_ptr<ImguiConsole> m_Console;
+
 	};
 }
