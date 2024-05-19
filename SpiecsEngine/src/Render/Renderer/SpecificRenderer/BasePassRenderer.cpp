b78272224e05e5f8818f456cd025dab9857db261
Spiecs <spiecs@outlook.com>
2024-05-19 23:25:10 +0800

    add basepassrenderer

---
+++ SpiecsEngine/src/Render/Renderer/SpecificRenderer/BasePassRenderer.cpp
@@ -0,0 +1,104 @@
+/**
+* @file BasePassRenderer.cpp.
+* @brief The BasePassRenderer Class Implementation.
+* @author Spiecs.
+*/
+
+#include "Pchheader.h"
+#include "BasePassRenderer.h"
+#include "PreRenderer.h"
+
+namespace Spiecs {
+
+	void BasePassRenderer::CreateRenderPass()
+	{
+		RendererPassBuilder{ "BassPass", this }
+		.AddSubPass("SkyBox")
+		.AddColorAttachment("Diffuse", [](bool& isEnableBlend, VkAttachmentDescription& description) {
+			description.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+			description.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		})
+		.AddColorAttachment("ID", [](bool& isEnableBlend, VkAttachmentDescription& description) {
+			description.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+			description.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+			description.format = VK_FORMAT_R32_SFLOAT;
+		})
+		.AddDepthAttachment([](VkAttachmentDescription& description) {
+			description.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+			description.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		})
+		.EndSubPass()
+		.AddSubPass("Mesh")
+		.AddColorAttachment("Diffuse", [](bool& isEnableBlend, VkAttachmentDescription& description) {})
+		.AddColorAttachment("Normal", [](bool& isEnableBlend, VkAttachmentDescription& description) {
+			description.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+			description.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		})
+		.AddColorAttachment("Specular", [](bool& isEnableBlend, VkAttachmentDescription& description) {
+			description.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+			description.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		})
+		.AddColorAttachment("ID", [](bool& isEnableBlend, VkAttachmentDescription& description) {
+			description.format = VK_FORMAT_R32_SFLOAT;
+		})
+		.AddDepthAttachment([](VkAttachmentDescription& description) {})
+		.EndSubPass()
+		.Build();
+	}
+
+	void BasePassRenderer::CreateDescriptorSet()
+	{
+		DescriptorSetBuilder{ this }
+			.AddPushConstant<PreR::PushConstant>()
+			.Build();
+	}
+
+	std::shared_ptr<VulkanPipeline> BasePassRenderer::CreatePipeline(
+		std::shared_ptr<Material> material,
+		VkRenderPass& renderPass,
+		VkPipelineLayout& layout
+	)
+	{
+		PipelineConfigInfo pipelineConfig{};
+		VulkanPipeline::DefaultPipelineConfigInfo(pipelineConfig);
+		pipelineConfig.renderPass = renderPass;
+		pipelineConfig.pipelineLayout = layout;
+		pipelineConfig.rasterizationInfo.cullMode = VK_CULL_MODE_FRONT_BIT;
+		pipelineConfig.colorBlendInfo.attachmentCount = (uint32_t)m_RenderPass->GetColorBlend().size();
+		pipelineConfig.colorBlendInfo.pAttachments = m_RenderPass->GetColorBlend().data();
+		return std::make_shared<VulkanPipeline>(
+			m_VulkanState,
+			GetSahderPath(material->GetShaderPath("vertShader"), "vert"),
+			GetSahderPath(material->GetShaderPath("fragShader"), "frag"),
+			pipelineConfig
+		);
+	}
+
+	void BasePassRenderer::Render(TimeStep& ts, FrameInfo& frameInfo)
+	{
+		RenderBehaverBuilder builder{ this ,frameInfo.m_FrameIndex, frameInfo.m_Imageindex };
+
+		builder.BeginRenderPass();
+
+		builder.BindDescriptorSet(DescriptorSetManager::GetByName("PreRenderer"));
+
+		IterWorldComp<SkyBoxComponent>(frameInfo, [&](int entityId, TransformComponent& transComp, SkyBoxComponent& skyboxComp) {
+			const glm::mat4& modelMatrix = transComp.GetModelMatrix();
+
+			skyboxComp.GetMesh()->Draw(m_VulkanState.m_CommandBuffer[frameInfo.m_FrameIndex], [&](uint32_t meshpackId, auto material) {
+				builder.BindPipeline(material->GetName());
+
+				builder.UpdatePushConstant<PreR::PushConstant>([&](auto& push) {
+					push.model = modelMatrix;
+					push.entityID = entityId;
+					});
+
+				builder.BindDescriptorSet(material->GetMaterialDescriptorSet(), material->GetName());
+				});
+
+			return false;
+			});
+
+		builder.EndRenderPass();
+	}
+}
---
+++ SpiecsEngine/src/Render/Renderer/SpecificRenderer/BasePassRenderer.h
@@ -0,0 +1,68 @@
+/**
+* @file BasePassRenderer.h.
+* @brief The BasePassRenderer Class Definitions.
+* @author Spiecs.
+*/
+
+#pragma once
+#include "Core/Core.h"
+#include "Render/Renderer/Renderer.h"
+
+namespace Spiecs {
+
+	/**
+	* @brief BasePassRenderer Class.
+	* This class defines the base pass render behaver.
+	*/
+	class BasePassRenderer : public Renderer
+	{
+	public:
+
+		/**
+		* @brief Constructor Function.
+		* Init member veriables.
+		* @param[in] rendererName The name of this Renderer.
+		* @param[in] vulkanState The core vulkan objects that in use.
+		* @param[in] desctiptorPool The DesctiptorPool.
+		*/
+		BasePassRenderer(
+			const std::string& rendererName,
+			VulkanState& vulkanState,
+			std::shared_ptr<VulkanDescriptorPool> desctiptorPool,
+			std::shared_ptr<VulkanDevice>         device,
+			std::shared_ptr<RendererResourcePool> rendererResourcePool
+		)
+			: Renderer(rendererName, vulkanState, desctiptorPool, device, rendererResourcePool)
+		{};
+
+		/**
+		* @brief Destructor Function.
+		*/
+		virtual ~BasePassRenderer() {};
+
+		/**
+		* @brief The interface is inherited from Renderer.
+		* @param[in] frameInfo The current frame data.
+		*/
+		virtual void Render(TimeStep& ts, FrameInfo& frameInfo) override;
+
+	private:
+
+		/**
+		* @brief The interface is inherited from Renderer.
+		* Create specific renderpass.
+		*/
+		virtual void CreateRenderPass() override;
+
+		virtual void CreateDescriptorSet() override;
+
+		virtual std::shared_ptr<VulkanPipeline> CreatePipeline(
+			std::shared_ptr<Material>  material,
+			VkRenderPass& renderPass,
+			VkPipelineLayout& layout
+		) override;
+
+		virtual void OnSlateResize() override { CreateRenderPass(); };
+	};
+
+}
