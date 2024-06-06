fdcd27a9c6a49174b55899d219c75ef7c20e722f
Spiecs <spiecs@outlook.com>
2024-06-06 14:16:05 +0800

    vulkanpipeline document

--- SpiecsEngine/src/Render/Vulkan/VulkanPipeline.cpp
+++ SpiecsEngine/src/Render/Vulkan/VulkanPipeline.cpp
@@ -1,3 +1,9 @@
+/**
+* @file VulkanPipeline.cpp.
+* @brief The VulkanPipeline Class Implementation.
+* @author Spiecs.
+*/
+
 #include "Pchheader.h"
 #include "VulkanPipeline.h"
 #include "Resources/Mesh/Mesh.h"
@@ -5,158 +11,225 @@
 namespace Spiecs {
 
 	VulkanPipeline::VulkanPipeline(
-		VulkanState& vulkanState, 
-		const std::string& pipelineName,
-		const std::string& vertFilepath, 
-		const std::string& fragFilepath, 
+		VulkanState&              vulkanState    , 
+		const std::string&        pipelineName   ,
+		const std::string&        vertShaderName ,
+		const std::string&        fragShaderName ,
 		const PipelineConfigInfo& config
 	)
 		: VulkanObject(vulkanState)
 	{
-		CreateGraphicsPipeline(pipelineName, vertFilepath, fragFilepath, config);
+		SPIECS_PROFILE_ZONE;
+
+		/**
+		* @brief Create Pipeline.
+		*/
+		CreateGraphicsPipeline(pipelineName, vertShaderName, fragShaderName, config);
 	}
 
 	VulkanPipeline::~VulkanPipeline()
 	{
+		SPIECS_PROFILE_ZONE;
+
+		/**
+		* @brief Destroy PipelineLayout.
+		*/
 		vkDestroyPipelineLayout(m_VulkanState.m_Device, m_PipelineLayout, nullptr);
+
+		/**
+		* @brief Destroy Pipeline.
+		*/
 		vkDestroyPipeline(m_VulkanState.m_Device, m_Pipeline, nullptr);
 	}
 
 	void VulkanPipeline::DefaultPipelineConfigInfo(PipelineConfigInfo& configInfo)
 	{
-		configInfo.inputAssemblyInfo = VkPipelineInputAssemblyStateCreateInfo{};
-		configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-		configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-		configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
-
-		configInfo.viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
-		configInfo.viewportInfo.viewportCount = 1;
-		configInfo.viewportInfo.pViewports = nullptr;
-		configInfo.viewportInfo.scissorCount = 1;
-		configInfo.viewportInfo.pScissors = nullptr;
-
-		configInfo.rasterizationInfo = VkPipelineRasterizationStateCreateInfo{};
-		configInfo.rasterizationInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-		configInfo.rasterizationInfo.depthClampEnable = VK_FALSE;
-		configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
-		configInfo.rasterizationInfo.polygonMode = VK_POLYGON_MODE_FILL;
-		configInfo.rasterizationInfo.lineWidth = 1.0f;
-		configInfo.rasterizationInfo.cullMode = VK_CULL_MODE_BACK_BIT;
-		configInfo.rasterizationInfo.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE; // negative clockwise
-		configInfo.rasterizationInfo.depthBiasEnable = VK_FALSE;
-		configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f;
-		configInfo.rasterizationInfo.depthBiasClamp = 0.0f;
-		configInfo.rasterizationInfo.depthBiasSlopeFactor = 0.0f;
-
-		configInfo.multisampleInfo = VkPipelineMultisampleStateCreateInfo{};
-		configInfo.multisampleInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-		configInfo.multisampleInfo.sampleShadingEnable = VK_FALSE;
-		configInfo.multisampleInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT; // TODO: Configurable, Disable MASS : VK_SAMPLE_COUNT_1_BIT, Enable MASS: VK_SAMPLE_COUNT_8_BIT.
-		configInfo.multisampleInfo.minSampleShading = 1.0f;
-		configInfo.multisampleInfo.pSampleMask = nullptr;
-		configInfo.multisampleInfo.alphaToCoverageEnable = VK_FALSE;
-		configInfo.multisampleInfo.alphaToOneEnable = VK_FALSE;
-
-		configInfo.colorBlendInfo = VkPipelineColorBlendStateCreateInfo{};
-		configInfo.colorBlendInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-		configInfo.colorBlendInfo.logicOpEnable = VK_FALSE;
-		configInfo.colorBlendInfo.logicOp = VK_LOGIC_OP_COPY;
+		SPIECS_PROFILE_ZONE;
 
 		/**
-		* @brief Need Rewrite.
+		* @brief VkPipelineInputAssemblyStateCreateInfo.
 		*/
-		configInfo.colorBlendInfo.attachmentCount = 0;
-		configInfo.colorBlendInfo.pAttachments = nullptr;
-		configInfo.colorBlendInfo.blendConstants[0] = 0.0f;
-		configInfo.colorBlendInfo.blendConstants[1] = 0.0f;
-		configInfo.colorBlendInfo.blendConstants[2] = 0.0f;
-		configInfo.colorBlendInfo.blendConstants[3] = 0.0f;
+		configInfo.inputAssemblyInfo                         = VkPipelineInputAssemblyStateCreateInfo{};
+		configInfo.inputAssemblyInfo.sType                   = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+		configInfo.inputAssemblyInfo.topology                = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+		configInfo.inputAssemblyInfo.primitiveRestartEnable  = VK_FALSE;
 
-		configInfo.depthStencilInfo = VkPipelineDepthStencilStateCreateInfo{};
-		configInfo.depthStencilInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
-		configInfo.depthStencilInfo.depthTestEnable = VK_TRUE;
-		configInfo.depthStencilInfo.depthWriteEnable = VK_TRUE;
-		configInfo.depthStencilInfo.depthCompareOp = VK_COMPARE_OP_LESS;
-		configInfo.depthStencilInfo.depthBoundsTestEnable = VK_FALSE;
-		configInfo.depthStencilInfo.minDepthBounds = 0.0f;
-		configInfo.depthStencilInfo.maxDepthBounds = 1.0f;
-		configInfo.depthStencilInfo.stencilTestEnable = VK_FALSE;
-		configInfo.depthStencilInfo.front = {};
-		configInfo.depthStencilInfo.back = {};
+		/**
+		* @brief VkPipelineViewportStateCreateInfo.
+		*/
+		configInfo.viewportInfo.sType                        = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+		configInfo.viewportInfo.viewportCount                = 1;
+		configInfo.viewportInfo.pViewports                   = nullptr;
+		configInfo.viewportInfo.scissorCount                 = 1;
+		configInfo.viewportInfo.pScissors                    = nullptr;
 
-		configInfo.dynamicStateEnables = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR };
-		configInfo.dynamicStateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
-		configInfo.dynamicStateInfo.pDynamicStates = configInfo.dynamicStateEnables.data();
-		configInfo.dynamicStateInfo.dynamicStateCount = static_cast<uint32_t>(configInfo.dynamicStateEnables.size());
-		configInfo.dynamicStateInfo.flags = 0;
+		/**
+		* @brief VkPipelineRasterizationStateCreateInfo.
+		*/
+		configInfo.rasterizationInfo                         = VkPipelineRasterizationStateCreateInfo{};
+		configInfo.rasterizationInfo.sType                   = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+		configInfo.rasterizationInfo.depthClampEnable        = VK_FALSE;
+		configInfo.rasterizationInfo.rasterizerDiscardEnable = VK_FALSE;
+		configInfo.rasterizationInfo.polygonMode             = VK_POLYGON_MODE_FILL;
+		configInfo.rasterizationInfo.lineWidth               = 1.0f;
+		configInfo.rasterizationInfo.cullMode                = VK_CULL_MODE_BACK_BIT;
+		configInfo.rasterizationInfo.frontFace               = VK_FRONT_FACE_COUNTER_CLOCKWISE; // negative clockwise
+		configInfo.rasterizationInfo.depthBiasEnable         = VK_FALSE;
+		configInfo.rasterizationInfo.depthBiasConstantFactor = 0.0f;
+		configInfo.rasterizationInfo.depthBiasClamp          = 0.0f;
+		configInfo.rasterizationInfo.depthBiasSlopeFactor    = 0.0f;
 
-		configInfo.bindingDescriptions = Vertex::GetBindingDescriptions();
-		configInfo.attributeDescriptions = Vertex::GetAttributeDescriptions();
+		/**
+		* @brief VkPipelineMultisampleStateCreateInfo.
+		*/
+		configInfo.multisampleInfo                           = VkPipelineMultisampleStateCreateInfo{};
+		configInfo.multisampleInfo.sType                     = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+		configInfo.multisampleInfo.sampleShadingEnable       = VK_FALSE;
+		configInfo.multisampleInfo.rasterizationSamples      = VK_SAMPLE_COUNT_1_BIT; // TODO: Configurable, Disable MASS : VK_SAMPLE_COUNT_1_BIT, Enable MASS: VK_SAMPLE_COUNT_8_BIT.
+		configInfo.multisampleInfo.minSampleShading          = 1.0f;
+		configInfo.multisampleInfo.pSampleMask               = nullptr;
+		configInfo.multisampleInfo.alphaToCoverageEnable     = VK_FALSE;
+		configInfo.multisampleInfo.alphaToOneEnable          = VK_FALSE;
+
+		/**
+		* @brief VkPipelineColorBlendStateCreateInfo.
+		*/
+		configInfo.colorBlendInfo                            = VkPipelineColorBlendStateCreateInfo{};
+		configInfo.colorBlendInfo.sType                      = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+		configInfo.colorBlendInfo.logicOpEnable              = VK_FALSE;
+		configInfo.colorBlendInfo.logicOp                    = VK_LOGIC_OP_COPY;
+
+		configInfo.colorBlendInfo.attachmentCount            = 0;
+		configInfo.colorBlendInfo.pAttachments               = nullptr;
+		configInfo.colorBlendInfo.blendConstants[0]          = 0.0f;
+		configInfo.colorBlendInfo.blendConstants[1]          = 0.0f;
+		configInfo.colorBlendInfo.blendConstants[2]          = 0.0f;
+		configInfo.colorBlendInfo.blendConstants[3]          = 0.0f;
+
+		/**
+		* @brief VkPipelineDepthStencilStateCreateInfo.
+		*/
+		configInfo.depthStencilInfo                          = VkPipelineDepthStencilStateCreateInfo{};
+		configInfo.depthStencilInfo.sType                    = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+		configInfo.depthStencilInfo.depthTestEnable          = VK_TRUE;
+		configInfo.depthStencilInfo.depthWriteEnable         = VK_TRUE;
+		configInfo.depthStencilInfo.depthCompareOp           = VK_COMPARE_OP_LESS;
+		configInfo.depthStencilInfo.depthBoundsTestEnable    = VK_FALSE;
+		configInfo.depthStencilInfo.minDepthBounds           = 0.0f;
+		configInfo.depthStencilInfo.maxDepthBounds           = 1.0f;
+		configInfo.depthStencilInfo.stencilTestEnable        = VK_FALSE;
+		configInfo.depthStencilInfo.front                    = {};
+		configInfo.depthStencilInfo.back                     = {};
+
+		/**
+		* @brief VkDynamicState.
+		*/
+		configInfo.dynamicStateEnables                       = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR };
+		configInfo.dynamicStateInfo.sType                    = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+		configInfo.dynamicStateInfo.pDynamicStates           = configInfo.dynamicStateEnables.data();
+		configInfo.dynamicStateInfo.dynamicStateCount        = static_cast<uint32_t>(configInfo.dynamicStateEnables.size());
+		configInfo.dynamicStateInfo.flags                    = 0;
+
+		/**
+		* @brief VkVertexInputBindingDescription.
+		*/
+		configInfo.bindingDescriptions                       = Vertex::GetBindingDescriptions();
+
+		/**
+		* @brief VkVertexInputAttributeDescription.
+		*/
+		configInfo.attributeDescriptions                     = Vertex::GetAttributeDescriptions();
 	}
 
 	void VulkanPipeline::Bind(uint32_t frameIndex)
 	{
+		SPIECS_PROFILE_ZONE;
+
+		/**
+		* @brief Bind Pipeline.
+		*/
 		vkCmdBindPipeline(m_VulkanState.m_CommandBuffer[frameIndex], VK_PIPELINE_BIND_POINT_GRAPHICS, m_Pipeline);
 	}
 
 	void VulkanPipeline::CreateGraphicsPipeline(
-		const std::string& pipelineName,
-		const std::string& vertShaderName,
-		const std::string& fragShaderName,
+		const std::string&        pipelineName    ,
+		const std::string&        vertShaderName  ,
+		const std::string&        fragShaderName  ,
 		const PipelineConfigInfo& config
 	)
 	{
+		SPIECS_PROFILE_ZONE;
+
+		/**
+		* @brief Receive PipelineLayout from parameter.
+		*/
 		m_PipelineLayout = config.pipelineLayout;
 
+		/**
+		* @brief Create the VulkanShaderModule.
+		*/
 		m_VertShaderModule = std::make_unique<VulkanShaderModule>(m_VulkanState, vertShaderName, "vert");
 		m_FragShaderModule = std::make_unique<VulkanShaderModule>(m_VulkanState, fragShaderName, "frag");
 
+		/**
+		* @brief Instance VkPipelineShaderStageCreateInfo.
+		*/
 		VkPipelineShaderStageCreateInfo shaderStages[2];
-		shaderStages[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-		shaderStages[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
-		shaderStages[0].module = m_VertShaderModule->Get();
-		shaderStages[0].pName = "main";
-		shaderStages[0].flags = 0;
-		shaderStages[0].pNext = nullptr;
-		shaderStages[0].pSpecializationInfo = nullptr;
+		shaderStages[0].sType                = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		shaderStages[0].stage                = VK_SHADER_STAGE_VERTEX_BIT;
+		shaderStages[0].module               = m_VertShaderModule->Get();
+		shaderStages[0].pName                = "main";
+		shaderStages[0].flags                = 0;
+		shaderStages[0].pNext                = nullptr;
+		shaderStages[0].pSpecializationInfo  = nullptr;
 
-		shaderStages[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-		shaderStages[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-		shaderStages[1].module = m_FragShaderModule->Get();
-		shaderStages[1].pName = "main";
-		shaderStages[1].flags = 0;
-		shaderStages[1].pNext = nullptr;
-		shaderStages[1].pSpecializationInfo = nullptr;
+		shaderStages[1].sType                = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		shaderStages[1].stage                = VK_SHADER_STAGE_FRAGMENT_BIT;
+		shaderStages[1].module               = m_FragShaderModule->Get();
+		shaderStages[1].pName                = "main";
+		shaderStages[1].flags                = 0;
+		shaderStages[1].pNext                = nullptr;
+		shaderStages[1].pSpecializationInfo  = nullptr;
 		
 		auto& bindingDescriptions = config.bindingDescriptions;
 		auto& attributeDescriptions = config.attributeDescriptions;
+
+		/**
+		* @brief Instance a VkPipelineVertexInputStateCreateInfo.
+		*/
 		VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
-		vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+		vertexInputInfo.sType                           = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
 		vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
-		vertexInputInfo.vertexBindingDescriptionCount = static_cast<uint32_t>(bindingDescriptions.size());
-		vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
-		vertexInputInfo.pVertexBindingDescriptions = bindingDescriptions.data();
+		vertexInputInfo.vertexBindingDescriptionCount   = static_cast<uint32_t>(bindingDescriptions.size());
+		vertexInputInfo.pVertexAttributeDescriptions    = attributeDescriptions.data();
+		vertexInputInfo.pVertexBindingDescriptions      = bindingDescriptions.data();
 
+		/**
+		* @brief Instance a VkGraphicsPipelineCreateInfo.
+		*/
 		VkGraphicsPipelineCreateInfo pipelineInfo{};
-		pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
-		pipelineInfo.stageCount = 2;
-		pipelineInfo.pStages = shaderStages;
-		pipelineInfo.pVertexInputState = &vertexInputInfo;
-		pipelineInfo.pInputAssemblyState = &config.inputAssemblyInfo;
-		pipelineInfo.pViewportState = &config.viewportInfo;
-		pipelineInfo.pRasterizationState = &config.rasterizationInfo;
-		pipelineInfo.pMultisampleState = &config.multisampleInfo;
-		pipelineInfo.pColorBlendState = &config.colorBlendInfo;
-		pipelineInfo.pDepthStencilState = &config.depthStencilInfo;
-		pipelineInfo.pDynamicState = &config.dynamicStateInfo;
+		pipelineInfo.sType                              = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+		pipelineInfo.stageCount                         = 2;
+		pipelineInfo.pStages                            = shaderStages;
+		pipelineInfo.pVertexInputState                  = &vertexInputInfo;
+		pipelineInfo.pInputAssemblyState                = &config.inputAssemblyInfo;
+		pipelineInfo.pViewportState                     = &config.viewportInfo;
+		pipelineInfo.pRasterizationState                = &config.rasterizationInfo;
+		pipelineInfo.pMultisampleState                  = &config.multisampleInfo;
+		pipelineInfo.pColorBlendState                   = &config.colorBlendInfo;
+		pipelineInfo.pDepthStencilState                 = &config.depthStencilInfo;
+		pipelineInfo.pDynamicState                      = &config.dynamicStateInfo;
 
-		pipelineInfo.layout = config.pipelineLayout;
-		pipelineInfo.renderPass = config.renderPass;
-		pipelineInfo.subpass = config.subpass;
+		pipelineInfo.layout                             = config.pipelineLayout;
+		pipelineInfo.renderPass                         = config.renderPass;
+		pipelineInfo.subpass                            = config.subpass;
 
-		pipelineInfo.basePipelineIndex = -1;
-		pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+		pipelineInfo.basePipelineIndex                  = -1;
+		pipelineInfo.basePipelineHandle                 = VK_NULL_HANDLE;
 
+		/**
+		* @brief Create Pipeline.
+		*/
 		VK_CHECK(vkCreateGraphicsPipelines(m_VulkanState.m_Device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &m_Pipeline));
 		VulkanDebugUtils::SetObjectName(VK_OBJECT_TYPE_PIPELINE, m_Pipeline, m_VulkanState.m_Device, pipelineName);
 	}
--- SpiecsEngine/src/Render/Vulkan/VulkanPipeline.h
+++ SpiecsEngine/src/Render/Vulkan/VulkanPipeline.h
@@ -1,3 +1,9 @@
+/**
+* @file VulkanPipeline.h.
+* @brief The VulkanPipeline Class Definitions.
+* @author Spiecs.
+*/
+
 #pragma once
 #include "Core/Core.h"
 #include "VulkanUtils.h"
@@ -5,53 +11,179 @@
 
 namespace Spiecs {
 
-	struct PipelineConfigInfo {
+	/**
+	* @brief This struct included all infos usd to create a VkPipeline.
+	*/
+	struct PipelineConfigInfo 
+	{
+		/**
+		* @brief Copy Constructor Function.
+		* @note This Class not allowed copy behaver.
+		*/
 		PipelineConfigInfo() = default;
+
+		/**
+		* @brief Copy Constructor Function.
+		* @note This Class not allowed copy behaver.
+		*/
 		PipelineConfigInfo(const PipelineConfigInfo&) = delete;
+
+		/**
+		* @brief Copy Assignment Operation.
+		* @note This Class not allowed copy behaver.
+		*/
 		PipelineConfigInfo& operator=(const PipelineConfigInfo&) = delete;
 
-		std::vector<VkVertexInputBindingDescription> bindingDescriptions{};
+		/**
+		* @brief VertexInputBindingDescription, from Vertex.
+		*/
+		std::vector<VkVertexInputBindingDescription>   bindingDescriptions{};
+
+		/**
+		* @brief VertexInputAttributeDescription, from Vertex.
+		*/
 		std::vector<VkVertexInputAttributeDescription> attributeDescriptions{};
-		VkPipelineViewportStateCreateInfo viewportInfo;
-		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
-		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
-		VkPipelineMultisampleStateCreateInfo multisampleInfo;
-		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
-		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
-		std::vector<VkDynamicState> dynamicStateEnables;
-		VkPipelineDynamicStateCreateInfo dynamicStateInfo;
-		VkPipelineLayout pipelineLayout = nullptr;
-		VkRenderPass renderPass = nullptr;
-		uint32_t subpass = 0;
+
+		/**
+		* @brief VkPipelineViewportStateCreateInfo.
+		*/
+		VkPipelineViewportStateCreateInfo              viewportInfo;
+
+		/**
+		* @brief VkPipelineInputAssemblyStateCreateInfo.
+		*/
+		VkPipelineInputAssemblyStateCreateInfo         inputAssemblyInfo;
+
+		/**
+		* @brief VkPipelineRasterizationStateCreateInfo.
+		*/
+		VkPipelineRasterizationStateCreateInfo         rasterizationInfo;
+
+		/**
+		* @brief VkPipelineMultisampleStateCreateInfo.
+		*/
+		VkPipelineMultisampleStateCreateInfo           multisampleInfo;
+
+		/**
+		* @brief VkPipelineColorBlendStateCreateInfo.
+		*/
+		VkPipelineColorBlendStateCreateInfo            colorBlendInfo;
+
+		/**
+		* @brief VkPipelineDepthStencilStateCreateInfo.
+		*/
+		VkPipelineDepthStencilStateCreateInfo          depthStencilInfo;
+
+		/**
+		* @brief VkDynamicState.
+		*/
+		std::vector<VkDynamicState>                    dynamicStateEnables;
+
+		/**
+		* @brief VkPipelineDynamicStateCreateInfo.
+		*/
+		VkPipelineDynamicStateCreateInfo               dynamicStateInfo;
+
+		/**
+		* @brief VkPipelineLayout.
+		*/
+		VkPipelineLayout                               pipelineLayout = nullptr;
+
+		/**
+		* @brief VkRenderPass.
+		*/
+		VkRenderPass                                   renderPass = nullptr;
+
+		/**
+		* @brief SubPass num.
+		*/
+		uint32_t                                       subpass = 0;
 	};
 
+	/**
+	* @brief This class is a wapper of VkPipelineLayout and VkPipeline.
+	*/
 	class VulkanPipeline : public VulkanObject
 	{
 	public:
+
+		/**
+		* @brief Constructor Function.
+		* Create VkPipeline.
+		* @param[in] vulkanState The global VulkanState.
+		* @param[in] pipelineName The Pipeline name.
+		* @param[in] vertShaderName The vert Shader name.
+		* @param[in] fragShaderName The frag Shader name.
+		* @param[in] config PipelineConfigInfo.
+		*/
 		VulkanPipeline(
-			VulkanState&               vulkanState  ,
-			const std::string&         pipelineName ,
-			const std::string&         vertFilepath , 
-			const std::string&         fragFilepath , 
+			VulkanState&               vulkanState   ,
+			const std::string&         pipelineName  ,
+			const std::string&         vertShaderName,
+			const std::string&         fragShaderName,
 			const PipelineConfigInfo&  config
 		);
 
+		/**
+		* @brief Destructor Function.
+		*/
 		virtual ~VulkanPipeline();
 
+		/**
+		* @brief Create a PipelineConfigInfo with default parameter.
+		* @param[in out] configInfo PipelineConfigInfo.
+		*/
 		static void DefaultPipelineConfigInfo(PipelineConfigInfo& configInfo);
 
+		/**
+		* @brief Bind Pipeline with commandbuffer index.
+		* @param[in] frameIndex The CommandBuffer Index.
+		*/
 		void Bind(uint32_t frameIndex);
-		VkPipelineLayout& GetPipelineLayout() { return m_PipelineLayout; };
+
+		/**
+		* @brief Get VkPipelineLayout.
+		* @return Returns the VkPipelineLayout.
+		*/
+		inline VkPipelineLayout& GetPipelineLayout() { return m_PipelineLayout; };
 
 	private:
-		void CreateGraphicsPipeline(const std::string& pipelineName, const std::string& vertShaderName, const std::string& fragShaderName, const PipelineConfigInfo& config);
+
+		/**
+		* @brief Create the VkPipeline.
+		* @param[in] pipelineName The Pipeline name.
+		* @param[in] vertShaderName The vert Shader name.
+		* @param[in] fragShaderName The frag Shader name.
+		* @param[in] config PipelineConfigInfo.
+		*/
+		void CreateGraphicsPipeline(
+			const std::string&        pipelineName   ,
+			const std::string&        vertShaderName , 
+			const std::string&        fragShaderName , 
+			const PipelineConfigInfo& config
+		);
 
 	private:
 
+		/**
+		* @brief The VkPipelineLayout.
+		* @attention Create by Renderer and destroy by this.
+		*/
 		VkPipelineLayout m_PipelineLayout;
+
+		/**
+		* @brief The VkPipeline.
+		*/
 		VkPipeline m_Pipeline;
 
+		/**
+		* @brief The uniqie pointer of vert VulkanShaderModule.
+		*/
 		std::unique_ptr<VulkanShaderModule> m_VertShaderModule;
+
+		/**
+		* @brief The uniqie pointer of frag VulkanShaderModule.
+		*/
 		std::unique_ptr<VulkanShaderModule> m_FragShaderModule;
 	};
 }
