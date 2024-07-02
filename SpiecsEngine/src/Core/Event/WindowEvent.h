0a76057c191194bb05ba7e34dafe6ff0ebdcc433
Spiecs <spiecs@outlook.com>
2024-07-02 17:22:04 +0800

    windows & world event document

--- SpiecsEngine/src/Core/Event/WindowEvent.h
+++ SpiecsEngine/src/Core/Event/WindowEvent.h
@@ -1,3 +1,9 @@
+/**
+* @file WindowEvent.h.
+* @brief The WindowEvent Class Definitions.
+* @author The Cherno.
+*/
+
 #pragma once
 #include "Core/Core.h"
 #include "Event.h"
@@ -5,65 +11,172 @@
 namespace Spiecs {
 
 	/**
-	* @brief Called when window resized.
+	* @brief This Class is inherited from Event Class.
+	* Called when window resized.
 	* This Event registed by glfw window.
 	*/
 	class WindowResizeEvent : public Event
 	{
 	public:
-		WindowResizeEvent(unsigned int width, unsigned int height)
-			: m_Width(width), m_Height(height) {}
 
-		inline unsigned int GetWidth() const { return m_Width; }
-		inline unsigned int GetHeight() const { return m_Height; }
+		/**
+		* @brief Constructor Function.
+		* @param[in] width New Viewport Size.
+		* @param[in] height New Viewport Size.
+		*/
+		WindowResizeEvent(uint32_t width, uint32_t height)
+			: m_Width(width)
+		    , m_Height(height)
+		{}
 
-		std::string ToString() const override
+		/**
+		* @brief Destructor Function.
+		*/
+		virtual ~WindowResizeEvent() override = default;
+
+		/**
+		* @brief Get New Windows Width.
+		* @return Returns New Windows Width.
+		*/
+		inline uint32_t GetWidth() const { return m_Width; }
+
+		/**
+		* @brief Get New Windows Height.
+		* @return Returns New Windows Height.
+		*/
+		inline uint32_t GetHeight() const { return m_Height; }
+
+		/**
+		* @brief Serialize this Event Class to string.
+		* @return Returns Serialized string.
+		*/
+		virtual std::string ToString() const override
 		{
 			std::stringstream ss;
 			ss << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+			
 			return ss.str();
 		}
 
+		/**
+		* @brief Specific this Class Type with WindowResize.
+		*/
 		EVENT_CLASS_TYPE(WindowResize)
+
+		/**
+		* @brief Specific this Class Category with EventCategoryApplication.
+		*/
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 
 	private:
-		unsigned int m_Width, m_Height;
+
+		/**
+		* @brief New Windows Width.
+		*/
+		uint32_t m_Width;
+
+		/**
+		* @brief New Windows Height.
+		*/
+		uint32_t m_Height;
 	};
 
+	/**
+	* @brief This Class is inherited from Event Class.
+	*/
 	class WindowCloseEvent : public Event
 	{
 	public:
-		WindowCloseEvent() {}
 
+		/**
+		* @brief Constructor Function.
+		*/
+		WindowCloseEvent() = default;
+
+		/**
+		* @brief Destructor Function.
+		*/
+		virtual ~WindowCloseEvent() override = default;
+		
+		/**
+		* @brief Specific this Class Type with WindowClose.
+		*/
 		EVENT_CLASS_TYPE(WindowClose)
+
+		/**
+		* @brief Specific this Class Category with EventCategoryApplication.
+		*/
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
 
 	/**
-	* @breif Called when window resized over.
+	* @breif This Class is inherited from Event Class.
+	* Called when window resized over.
 	* This Event registed by VkSwapchain.
 	*/
 	class WindowResizeOverEvent : public Event
 	{
 	public:
-		WindowResizeOverEvent(unsigned int width, unsigned int height)
-			: m_Width(width), m_Height(height) {}
 
-		inline unsigned int GetWidth() const { return m_Width; }
-		inline unsigned int GetHeight() const { return m_Height; }
+		/**
+		* @brief Constructor Function.
+		* @param[in] width New Viewport Size.
+		* @param[in] height New Viewport Size.
+		*/
+		WindowResizeOverEvent(uint32_t width, uint32_t height)
+			: m_Width(width)
+		    , m_Height(height)
+		{}
 
-		std::string ToString() const override
+		/**
+		* @brief Destructor Function.
+		*/
+		virtual ~WindowResizeOverEvent() override = default;
+
+		/**
+		* @brief Get New Windows Width.
+		* @return Returns New Windows Width.
+		*/
+		inline uint32_t GetWidth() const { return m_Width; }
+
+		/**
+		* @brief Get New Windows Height.
+		* @return Returns New Windows Height.
+		*/
+		inline uint32_t GetHeight() const { return m_Height; }
+
+		/**
+		* @brief Serialize this Event Class to string.
+		* @return Returns Serialized string.
+		*/
+		virtual std::string ToString() const override
 		{
 			std::stringstream ss;
 			ss << "WindowResizeOverEvent: " << m_Width << ", " << m_Height;
+			
 			return ss.str();
 		}
 
+		/**
+		* @brief Specific this Class Type with WindowResizeOver.
+		*/
 		EVENT_CLASS_TYPE(WindowResizeOver)
+
+		/**
+		* @brief Specific this Class Category with EventCategoryApplication.
+		*/
 		EVENT_CLASS_CATEGORY(EventCategoryApplication)
 
 	private:
-		unsigned int m_Width, m_Height;
+
+		/**
+		* @brief New Windows Width.
+		*/
+		uint32_t m_Width;
+
+		/**
+		* @brief New Windows Height.
+		*/
+		uint32_t m_Height;
 	};
 }
--- SpiecsEngine/src/Core/Event/WorldEvent.h
+++ SpiecsEngine/src/Core/Event/WorldEvent.h
@@ -1,3 +1,9 @@
+/**
+* @file WorldEvent.h.
+* @brief The WorldEvent Class Definitions.
+* @author The Cherno.
+*/
+
 #pragma once
 #include "Core/Core.h"
 #include "Event.h"
@@ -5,30 +11,63 @@
 namespace Spiecs {
 
 	/**
-	* @brief Called when WorldMarkQueryer tick.
+	* @brief This Class is inherited from Event Class.
+	* Inherit from it and create specific KeyEvent class. 
+	* Called when WorldMarkQueryer tick.
 	* This Event registed by ...
 	*/
 	class WorldEvent : public Event
 	{
 	public:
-		WorldEvent() {};
 
+		/**
+		* @brief Constructor Function.
+		*/
+		WorldEvent() = default;
+
+		/**
+		* @brief Destructor Function.
+		*/
+		virtual ~WorldEvent() override = default;
+
+		/**
+		* @brief Specific this Class Category with EventCategoryWorld.
+		*/
 		EVENT_CLASS_CATEGORY(EventCategoryWorld)
 	};
 
+	/**
+	* @brief This Class is inherited from Event Class.
+	*/
 	class MeshAddedWorldEvent : public WorldEvent
 	{
 	public:
-		MeshAddedWorldEvent() {};
 
-		std::string ToString() const override
+		/**
+		* @brief Constructor Function.
+		*/
+		MeshAddedWorldEvent() = default;
+
+		/**
+		* @brief Destructor Function.
+		*/
+		virtual ~MeshAddedWorldEvent() override = default;
+
+		/**
+		* @brief Serialize this Event Class to string.
+		* @return Returns Serialized string.
+		*/
+		virtual std::string ToString() const override
 		{
 			std::stringstream ss;
 			ss << "MeshAddedWorldEvent";
+			
 			return ss.str();
 		}
 
+		/**
+		* @brief Specific this Class Type with MeshAdded.
+		*/
 		EVENT_CLASS_TYPE(MeshAdded)
-		EVENT_CLASS_CATEGORY(EventCategoryWorld)
 	};
 }
