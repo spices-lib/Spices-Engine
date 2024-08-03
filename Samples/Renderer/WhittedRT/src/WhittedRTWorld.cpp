3c40c7390154aa4e447c593c3dd6b1c2bb3df8a1
Spiecs <spiecs@outlook.com>
2024-08-04 00:12:54 +0800

    Update WhittedRTWorld.cpp

--- Samples/Renderer/WhittedRT/src/WhittedRTWorld.cpp
+++ Samples/Renderer/WhittedRT/src/WhittedRTWorld.cpp
@@ -27,6 +27,89 @@ namespace Spices {
 			cameraentity.AddComponent<NativeScriptComponent>(std::make_shared<CameraController>());
 		}
 
+		{
+			Entity plane = CreateEntity("CornellBox_RightFace");
+			TransformComponent& transformComp = plane.GetComponent<TransformComponent>();
+			MeshComponent& meshComp = plane.AddComponent<MeshComponent>();
+			transformComp.SetPosition({ 0.0f, 0.0f, 0.5f });
+			transformComp.SetRotation({ 0.0f, 0.0f, 0.0f });
+
+			std::shared_ptr<SquarePack> pack = std::make_shared<SquarePack>();
+
+			pack->SetMaterial("BasePassRenderer.Mesh.ground");
+			std::shared_ptr<Mesh> mesh = Mesh::Builder().AddPack(pack).Build();
+			meshComp.SetMesh(mesh);
+		}
+
+		{
+			Entity plane = CreateEntity("CornellBox_LeftFace");
+			TransformComponent& transformComp = plane.GetComponent<TransformComponent>();
+			MeshComponent& meshComp = plane.AddComponent<MeshComponent>();
+			transformComp.SetPosition({ 0.0f, 0.0f, -0.5f });
+			transformComp.SetRotation({ -180.0f, 0.0f, -180.0f });
+
+			std::shared_ptr<SquarePack> pack = std::make_shared<SquarePack>();
+
+			pack->SetMaterial("BasePassRenderer.Mesh.ground");
+			std::shared_ptr<Mesh> mesh = Mesh::Builder().AddPack(pack).Build();
+			meshComp.SetMesh(mesh);
+		}
+
+		{
+			Entity plane = CreateEntity("CornellBox_FrontFace");
+			TransformComponent& transformComp = plane.GetComponent<TransformComponent>();
+			MeshComponent& meshComp = plane.AddComponent<MeshComponent>();
+			transformComp.SetPosition({ -0.5f, 0.0f, 0.0f });
+			transformComp.SetRotation({ -180.0f, -90.0f, -180.0f });
+
+			std::shared_ptr<SquarePack> pack = std::make_shared<SquarePack>();
+
+			pack->SetMaterial("BasePassRenderer.Mesh.ground");
+			std::shared_ptr<Mesh> mesh = Mesh::Builder().AddPack(pack).Build();
+			meshComp.SetMesh(mesh);
+		}
+
+		{
+			Entity plane = CreateEntity("CornellBox_TopFace");
+			TransformComponent& transformComp = plane.GetComponent<TransformComponent>();
+			MeshComponent& meshComp = plane.AddComponent<MeshComponent>();
+			transformComp.SetPosition({ 0.0f, 0.5f, 0.0f });
+			transformComp.SetRotation({ 90.0f, -90.0f, 180.0f });
+
+			std::shared_ptr<SquarePack> pack = std::make_shared<SquarePack>();
+
+			pack->SetMaterial("BasePassRenderer.Mesh.ground");
+			std::shared_ptr<Mesh> mesh = Mesh::Builder().AddPack(pack).Build();
+			meshComp.SetMesh(mesh);
+		}
+
+		{
+			Entity plane = CreateEntity("CornellBox_ButtomFace");
+			TransformComponent& transformComp = plane.GetComponent<TransformComponent>();
+			MeshComponent& meshComp = plane.AddComponent<MeshComponent>();
+			transformComp.SetPosition({ 0.0f, -0.5f, 0.0f });
+			transformComp.SetRotation({ -90.0f, -90.0f, 180.0f });
+
+			std::shared_ptr<SquarePack> pack = std::make_shared<SquarePack>();
+
+			pack->SetMaterial("BasePassRenderer.Mesh.ground");
+			std::shared_ptr<Mesh> mesh = Mesh::Builder().AddPack(pack).Build();
+			meshComp.SetMesh(mesh);
+		}
+
+		{
+			Entity sphere = CreateEntity("CornellBox_Sphere");
+			MeshComponent& meshComp = sphere.AddComponent<MeshComponent>();
+			TransformComponent& transformComp = sphere.GetComponent<TransformComponent>();
+			transformComp.SetPosition({ 0.0f,  0.0f, 0.0f });
+			transformComp.SetScale({ 0.14f, 0.14f, 0.14f });
+
+			std::shared_ptr<SpherePack> pack = std::make_shared<SpherePack>(100, 100);
+
+			pack->SetMaterial("BasePassRenderer.Mesh.ground");
+			std::shared_ptr<Mesh> mesh = Mesh::Builder().AddPack(pack).Build();
+			meshComp.SetMesh(mesh);
+		}
 	}
 
 	void WhittedRTWorld::OnActivate(TimeStep& ts)
