14a46c4f10ee58c73a9beab0e4c3109995e12d46
Spiecs <spiecs@outlook.com>
2024-11-27 23:47:59 +0800

    Create BehaveStateGraph.h

---
+++ SpicesEngine/src/Core/Container/BehaveStateGraph.h
@@ -0,0 +1,31 @@
+/**
+* @file BehaveStateGraph.h.
+* @brief The BehaveStateGraph Class Definitions and Implementation.
+* @author Spices.
+*/
+
+#pragma once
+
+#include "Core/Core.h"
+
+#include <unordered_map>
+#include <list>
+
+namespace scl {
+
+	/**
+	* @brief
+	*/
+	template<typename T>
+	class behave_state_graph
+	{
+	private:
+
+
+
+	public:
+
+
+	};
+
+}
