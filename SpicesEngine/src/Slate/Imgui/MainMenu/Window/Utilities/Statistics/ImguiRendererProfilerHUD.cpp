a46b35dc9aad052f6a01a7f8c992b7474d4b3578
Spiecs <spiecs@outlook.com>
2024-12-01 21:54:35 +0800

    Update ImguiRendererProfilerHUD.cpp

--- SpicesEngine/src/Slate/Imgui/MainMenu/Window/Utilities/Statistics/ImguiRendererProfilerHUD.cpp
+++ SpicesEngine/src/Slate/Imgui/MainMenu/Window/Utilities/Statistics/ImguiRendererProfilerHUD.cpp
@@ -172,15 +172,24 @@ namespace Spices {
     {
         SPICES_PROFILE_ZONE;
 
+        /**
+        * @brief Tree node.
+        */
         struct TimestampResult
         {
             TimestampQueryer::Result result;
             std::string name;
         };
 
+        /**
+        * @brief Instance a Tree.
+        */
         scl::tree<TimestampResult> totalResult;
         totalResult.GetData().name = "Scene ( Viewport 0 )";
 
+        /**
+        * @brief Write scene data to tree.
+        */
         {
             SPICES_PROFILE_ZONEN("Fetch Statistics Caches");
 
@@ -219,8 +228,95 @@ namespace Spices {
         {
             SPICES_PROFILE_ZONEN("Draw Statistics Caches");
 
-            static ImGuiTableFlags flags = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable | ImGuiTableFlags_NoBordersInBody;
+            static ImGuiTableFlags flags = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable | ImGuiTableFlags_NoBordersInBody | 
+                                           ImGuiTableFlags_Sortable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_SortMulti | ImGuiTableFlags_Hideable |
+                                           ImGuiTableFlags_ScrollY | ImGuiTableFlags_SortTristate;
             static ImGuiTreeNodeFlags tree_node_flags = ImGuiTreeNodeFlags_SpanAllColumns;
+
+            /**
+            * @brief 0: NoSort, 1: Ascending, 2: Descending.
+            */
+            static int sortState = 0;
+
+            /**
+            * @brief Function of quick sort to PipelineResult.
+            */
+            static auto SortTimestampTreeMethod = [](const void* lhs, const void* rhs) -> int {
+                
+                const auto l = (const std::unique_ptr<scl::tree<TimestampResult>>*)lhs;
+                const auto r = (const std::unique_ptr<scl::tree<TimestampResult>>*)rhs;
+
+                ImGuiTableSortSpecs* sort_specs = ImGui::TableGetSortSpecs();
+                if (sortState == ImGuiSortDirection_Ascending)
+                {
+                    return ((*l)->GetData().result.timeStamp - (*r)->GetData().result.timeStamp) * 1000;
+                }
+                else
+                {
+                    return ((*r)->GetData().result.timeStamp - (*l)->GetData().result.timeStamp) * 1000;
+                }
+            };
+
+            /**
+            * @brief Function of sort whole tree.
+            */
+            static std::function<void(scl::tree<TimestampResult>*)> SortTimestampTree = [&](scl::tree<TimestampResult>* node) {
+
+                if (sortState == 0) return;
+
+                qsort((void*)node->GetChilds().data(), (size_t)node->GetChilds().size(), sizeof(node->GetChilds()[0]), SortTimestampTreeMethod);
+
+                for (auto& child : node->GetChilds())
+                {
+                    SortTimestampTree(child.get());
+                }
+            };
+
+            /**
+            * @brief Function of draw whole tree.
+            */
+            static std::function<void(scl::tree<TimestampResult>*, scl::tree<TimestampResult>*)> DrawTimestampTree = [&](scl::tree<TimestampResult>* node, scl::tree<TimestampResult>* root) {
+            
+                if (!node->GetData().result.valid) return;
+
+                ImGui::TableNextRow();
+                ImGui::TableNextColumn();
+                bool hasChild = node->GetChilds().size() > 0;
+                if (hasChild)
+                {
+                    bool open = ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags);
+                    ImGui::TableNextColumn();
+
+                    ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
+                    ImGui::ProgressBar(root->GetData().result.timeStamp > 0 ? (node->GetData().result.timeStamp / root->GetData().result.timeStamp) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
+                    ImGui::PopStyleColor();
+                    ImGui::SameLine(0.05f);
+                    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
+                    ImGui::Text("%.2f ms", node->GetData().result.timeStamp);
+                    ImGui::PopStyleColor();
+                    if (open)
+                    {
+                        for (auto& child : node->GetChilds())
+                        {
+                            DrawTimestampTree(child.get(), root);
+                        }
+                        ImGui::TreePop();
+                    }
+                }
+                else
+                {
+                    ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen);
+                    ImGui::TableNextColumn();
+                         
+                    ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
+                    ImGui::ProgressBar(root->GetData().result.timeStamp > 0 ? (node->GetData().result.timeStamp / root->GetData().result.timeStamp) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
+                    ImGui::PopStyleColor();
+                    ImGui::SameLine(0.05f);
+                    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
+                    ImGui::Text("%.2f ms", node->GetData().result.timeStamp);
+                    ImGui::PopStyleColor();
+                }
+            };
 
             ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 0.0f);
             ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.196f, 0.204f, 0.2f, 1.0f));
@@ -235,50 +331,25 @@ namespace Spices {
                 ImGui::TableSetupColumn("TimeCost", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_PreferSortDescending, ImGuiH::GetLineItemSize().x * 3.0f);
                 ImGui::TableHeadersRow();
 
-                static std::function<void(scl::tree<TimestampResult>*, scl::tree<TimestampResult>*)> DrawTimestampTree = [](scl::tree<TimestampResult>* node, scl::tree<TimestampResult>* root) {
-            
-                    if (!node->GetData().result.valid) return;
-                    if (node->GetData().result.timeStamp < 0.001f) return;
-
-                    ImGui::TableNextRow();
-                    ImGui::TableNextColumn();
-                    bool hasChild = node->GetChilds().size() > 0;
-                    if (hasChild)
+                // Sort our data if sort specs have been changed!
+                if (ImGuiTableSortSpecs* sort_specs = ImGui::TableGetSortSpecs())
+                {
+                    // State changed since last frame.
+                    if (sort_specs->SpecsDirty)
                     {
-                        bool open = ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags);
-                        ImGui::TableNextColumn();
+                        sortState = 0;
 
-                        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
-                        ImGui::ProgressBar(root->GetData().result.timeStamp > 0.01f ? (node->GetData().result.timeStamp  / root->GetData().result.timeStamp) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
-                        ImGui::PopStyleColor();
-                        ImGui::SameLine(0.05f);
-                        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
-                        ImGui::Text("%.2f ms", node->GetData().result.timeStamp);
-                        ImGui::PopStyleColor();
-                        if (open)
+                        // Need sort this frame.
+                        if (sort_specs->SpecsCount > 0)
                         {
-                            for (auto& child : node->GetChilds())
-                            {
-                                DrawTimestampTree(child.get(), root);
-                            }
-                            ImGui::TreePop();
+                            sortState = sort_specs->Specs->SortDirection;
                         }
-                    }
-                    else
-                    {
-                        ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen);
-                        ImGui::TableNextColumn();
 
-                        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
-                        ImGui::ProgressBar(root->GetData().result.timeStamp > 0.01f ? (node->GetData().result.timeStamp / root->GetData().result.timeStamp) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
-                        ImGui::PopStyleColor();
-                        ImGui::SameLine(0.05f);
-                        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
-                        ImGui::Text("%.2f ms", node->GetData().result.timeStamp);
-                        ImGui::PopStyleColor();
+                        sort_specs->SpecsDirty = false;
                     }
-                };
+                }
 
+                SortTimestampTree(&totalResult);
                 DrawTimestampTree(&totalResult, &totalResult);
 
                 ImGui::EndTable();
@@ -292,15 +363,30 @@ namespace Spices {
     {
         SPICES_PROFILE_ZONE;
 
+        /**
+        * @brief Store bit as static variable.
+        */
+        static int selectBit;
+        selectBit = bit;
+
+        /**
+        * @brief Tree node.
+        */
         struct PipelineResult
         {
             PipelineStatisticsQueryer::Result result;
             std::string name;
         };
 
+        /**
+        * @brief Instance a Tree.
+        */
         scl::tree<PipelineResult> totalResult;
         totalResult.GetData().name = "Scene ( Viewport 0 )";
 
+        /**
+        * @brief Write scene data to tree.
+        */
         {
             SPICES_PROFILE_ZONEN("Fetch Statistics Caches");
 
@@ -339,8 +425,95 @@ namespace Spices {
         {
             SPICES_PROFILE_ZONEN("Draw Statistics Caches");
 
-            static ImGuiTableFlags flags = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable | ImGuiTableFlags_NoBordersInBody;
+            static ImGuiTableFlags flags = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable | ImGuiTableFlags_NoBordersInBody | 
+                                           ImGuiTableFlags_Sortable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_SortMulti | ImGuiTableFlags_Hideable |
+                                           ImGuiTableFlags_ScrollY | ImGuiTableFlags_SortTristate;
             static ImGuiTreeNodeFlags tree_node_flags = ImGuiTreeNodeFlags_SpanAllColumns;
+
+            /**
+            * @brief 0: NoSort, 1: Ascending, 2: Descending.
+            */
+            static int sortState = 0;
+
+            /**
+            * @brief Function of quick sort to PipelineResult.
+            */
+            static auto SortTimestampTreeMethod = [](const void* lhs, const void* rhs) -> int {
+                
+                const auto l = (const std::unique_ptr<scl::tree<PipelineResult>>*)lhs;
+                const auto r = (const std::unique_ptr<scl::tree<PipelineResult>>*)rhs;
+
+                ImGuiTableSortSpecs* sort_specs = ImGui::TableGetSortSpecs();
+                if (sortState == ImGuiSortDirection_Ascending)
+                {
+                    return (*l)->GetData().result.statistics[selectBit] - (*r)->GetData().result.statistics[selectBit];
+                }
+                else
+                {
+                    return (*r)->GetData().result.statistics[selectBit] - (*l)->GetData().result.statistics[selectBit];
+                }
+            };
+
+            /**
+            * @brief Function of sort whole tree.
+            */
+            static std::function<void(scl::tree<PipelineResult>*)> SortTimestampTree = [&](scl::tree<PipelineResult>* node) {
+
+                if (sortState == 0) return;
+
+                qsort((void*)node->GetChilds().data(), (size_t)node->GetChilds().size(), sizeof(node->GetChilds()[0]), SortTimestampTreeMethod);
+
+                for (auto& child : node->GetChilds())
+                {
+                    SortTimestampTree(child.get());
+                }
+            };
+
+            /**
+            * @brief Function of draw whole tree.
+            */
+            static std::function<void(scl::tree<PipelineResult>*, scl::tree<PipelineResult>*)> DrawTimestampTree = [&](scl::tree<PipelineResult>* node, scl::tree<PipelineResult>* root) {
+            
+                if (!node->GetData().result.valid) return;
+
+                ImGui::TableNextRow();
+                ImGui::TableNextColumn();
+                bool hasChild = node->GetChilds().size() > 0;
+                if (hasChild)
+                {
+                    bool open = ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags);
+                    ImGui::TableNextColumn();
+
+                    ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
+                    ImGui::ProgressBar(root->GetData().result.statistics[bit] > 0 ? (node->GetData().result.statistics[bit] / float(root->GetData().result.statistics[bit])) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
+                    ImGui::PopStyleColor();
+                    ImGui::SameLine(0.05f);
+                    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
+                    ImGui::Text("%d", node->GetData().result.statistics[bit]);
+                    ImGui::PopStyleColor();
+                    if (open)
+                    {
+                        for (auto& child : node->GetChilds())
+                        {
+                            DrawTimestampTree(child.get(), root);
+                        }
+                        ImGui::TreePop();
+                    }
+                }
+                else
+                {
+                    ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen);
+                    ImGui::TableNextColumn();
+                         
+                    ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
+                    ImGui::ProgressBar(root->GetData().result.statistics[bit] > 0 ? (node->GetData().result.statistics[bit] / float(root->GetData().result.statistics[bit])) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
+                    ImGui::PopStyleColor();
+                    ImGui::SameLine(0.05f);
+                    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
+                    ImGui::Text("%d", node->GetData().result.statistics[bit]);
+                    ImGui::PopStyleColor();
+                }
+            };
 
             ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 0.0f);
             ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.196f, 0.204f, 0.2f, 1.0f));
@@ -351,53 +524,30 @@ namespace Spices {
             if (ImGui::BeginTable("pipelineTree", 2, flags))
             {
                 // The first column will use the default _WidthStretch when ScrollX is Off and _WidthFixed when ScrollX is On
-                ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_NoHide);
+                ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_NoSort | ImGuiTableColumnFlags_NoHide);
                 ImGui::TableSetupColumn("Invocation Count", ImGuiTableColumnFlags_WidthFixed | ImGuiTableColumnFlags_PreferSortDescending, ImGuiH::GetLineItemSize().x * 3.0f);
+                ImGui::TableSetupScrollFreeze(0, 1);
                 ImGui::TableHeadersRow();
 
-                static std::function<void(scl::tree<PipelineResult>*, scl::tree<PipelineResult>*)> DrawTimestampTree = [&](scl::tree<PipelineResult>* node, scl::tree<PipelineResult>* root) {
-            
-                    if (!node->GetData().result.valid) return;
-
-                    ImGui::TableNextRow();
-                    ImGui::TableNextColumn();
-                    bool hasChild = node->GetChilds().size() > 0;
-                    if (hasChild)
+                // Sort our data if sort specs have been changed!
+                if (ImGuiTableSortSpecs* sort_specs = ImGui::TableGetSortSpecs())
+                {
+                    // State changed since last frame.
+                    if (sort_specs->SpecsDirty)
                     {
-                        bool open = ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags);
-                        ImGui::TableNextColumn();
+                        sortState = 0;
 
-                        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
-                        ImGui::ProgressBar(root->GetData().result.statistics[bit] > 0 ? (node->GetData().result.statistics[bit] / float(root->GetData().result.statistics[bit])) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
-                        ImGui::PopStyleColor();
-                        ImGui::SameLine(0.05f);
-                        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
-                        ImGui::Text("%d", node->GetData().result.statistics[bit]);
-                        ImGui::PopStyleColor();
-                        if (open)
+                        // Need sort this frame.
+                        if (sort_specs->SpecsCount > 0)
                         {
-                            for (auto& child : node->GetChilds())
-                            {
-                                DrawTimestampTree(child.get(), root);
-                            }
-                            ImGui::TreePop();
+                            sortState = sort_specs->Specs->SortDirection;
                         }
-                    }
-                    else
-                    {
-                        ImGui::TreeNodeEx(node->GetData().name.c_str(), tree_node_flags | ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen);
-                        ImGui::TableNextColumn();
-                         
-                        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.8f, 0.0f, 1.0f));
-                        ImGui::ProgressBar(root->GetData().result.statistics[bit] > 0 ? (node->GetData().result.statistics[bit] / float(root->GetData().result.statistics[bit])) : 0.0f, ImVec2(-FLT_MIN, 2.0f), "##");
-                        ImGui::PopStyleColor();
-                        ImGui::SameLine(0.05f);
-                        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.8f, 0.8f, 0.8f, 1.0f));
-                        ImGui::Text("%d", node->GetData().result.statistics[bit]);
-                        ImGui::PopStyleColor();
-                    }
-                };
 
+                        sort_specs->SpecsDirty = false;
+                    }
+                }
+
+                SortTimestampTree(&totalResult);
                 DrawTimestampTree(&totalResult, &totalResult);
 
                 ImGui::EndTable();
