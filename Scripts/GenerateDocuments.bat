59ac5e6d40b734bb37d48a096f60d690a1dea151
Spiecs <spiecs@outlook.com>
2024-12-31 14:50:10 +0800

    Update GenerateDocuments.bat

--- Scripts/GenerateDocuments.bat
+++ Scripts/GenerateDocuments.bat
@@ -6,24 +6,35 @@ set "parent_dir=%cd%\.."
 REM Display parent directory for debugging
 echo Parent directory: %parent_dir%
 
-REM Fetch solution path.
-for %%f in ("%parent_dir%\Doxyfile") do set "doxyfile_path=%%f"
-
 REM Display doxyfile path for debugging
 echo Doxyfile path: %doxyfile_path%
 
 REM List files in the parent directory for debugging
 dir "%parent_dir%"
 
-REM Return if not find a solution file
-if not defined doxyfile_path (
-    echo doxyfile is not found
-    PAUSE
-    exit /b 1
-)
+REM Check if doxyfile exists in the parent directory
+if exist "%parent_dir%\Doxyfile" (
 
-REM Back to parent folder
-cd ../
+    set "doxyfile_path=%parent_dir%\Doxyfile"
+    
+    REM Back to parent folder
+    cd ../
+        
+) else (
+
+    REM Check if doxyfile exists in the current directory
+    if exist "Doxyfile" (
+    
+        set "doxyfile_path=%cd%\Doxyfile"
+        
+    ) else (
+    
+        echo Doxyfile not found.
+        PAUSE
+        exit /b 1
+        
+    )
+)
 
 REM Call Python script to build solution
 doxygen %doxyfile_path%
